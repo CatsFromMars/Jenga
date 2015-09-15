6174d635a491352a219b85c6d01b5fa9811f08b2
Ivan Wang <icw@cmu.edu>
2015-09-15 10:16:03 -0400

    add basic GameState script

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+    public enum State {
+        Taking,   // removing a piece from the Jenga tower
+        Placing,  // placing a piece on the Jenga tower
+        GameOver, // everything collapsed
+    };
+
+    public static State currentState = State.Taking;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+}
