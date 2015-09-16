4385a728f774465370a487b7a97e9796c7d8b8e3
CatsFromMars <mduque@andrew.cmu.edu>
2015-09-16 17:32:17 -0400

    block movement added

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private Rigidbody rb;
+	Transform cursor;
+	
+	void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		rb = GetComponent <Rigidbody>();
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		cursor = GameObject.FindGameObjectWithTag ("Cursor").transform;
+	}
+	
+	void OnMouseDrag()
+	{
+		transform.parent = cursor;
+	}
+
+	void OnMouseUp() {
+		transform.parent = null;
+	}
+}
---
+++ Assets/Scripts/Finger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finger : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private Rigidbody rb;
+	
+	void Awake()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		rb = GetComponent <Rigidbody>();
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	void Update()
+	{
+		Debug.Log ("MOVING!");
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+		transform.position = curPosition;
+		
+	}
+}
