fa4ad2a014935211ca985f753f1f0b375965899a
Ivan Wang <icw@cmu.edu>
2015-09-16 12:25:10 -0400

    hide finger when switching states; add cheats for testing

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -8,14 +8,69 @@ public class GameState : MonoBehaviour {
         GameOver, // everything collapsed
     };
 
-    public static State currentState = State.Taking;
+    private static GameState gameState;
+
+    private State currentState;
+    public GameObject finger;
+    public bool enableCheats;
+
+    public static State CurrentState {
+        get {
+            return gameState.currentState;
+        }
+    }
+
+    public static GameObject Finger {
+        get {
+            return gameState.finger;
+        }
+    }
+
+    // Handle state change logic
+    public static void ChangeState(State newState) {
+        switch (newState) {
+            case State.Taking:
+                Finger.SetActive(true);
+                break;
+
+            case State.Placing:
+                Finger.SetActive(false);
+                break;
+
+            case State.GameOver:
+                Finger.SetActive(false);
+                // TODO(jason): slow motion, play music
+                break;
+        }
+
+        gameState.currentState = newState;
+    }
+
+    void Awake() {
+        gameState = this;
+    }
 
     // Use this for initialization
     void Start () {
+        gameState.finger = finger;
     }
 
     // Update is called once per frame
     void Update () {
+        if (!enableCheats) {
+            return;
+        }
+
+        if (Input.GetKeyDown("1")) {
+            Debug.Log("switching to state: taking");
+            ChangeState(State.Taking);
+        } else if (Input.GetKeyDown("2")) {
+            Debug.Log("switching to state: placing");
+            ChangeState(State.Placing);
+        } else if (Input.GetKeyDown("3")) {
+            Debug.Log("switching to state: gameover");
+            ChangeState(State.GameOver);
+        }
     }
 
 }
