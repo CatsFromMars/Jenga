10340314232bf0550788cb512a535de5d00ae810
Ivan Wang <icw@cmu.edu>
2015-09-16 22:24:22 -0400

    update cube prefab, placing block script

---
+++ Assets/Scripts/PlaceBlock.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceBlock : MonoBehaviour {
+	public GameObject block;
+
+	//private Vector3 screenPoint;
+	//private Vector3 offset;
+	//private Rigidbody rb;
+	//Transform cursor;
+	MeshRenderer r;
+	private CameraControl cam;
+
+	void Awake() {
+		r = GetComponent<MeshRenderer>();
+		cam = Camera.main.GetComponent<CameraControl>();
+	}
+
+	void OnMouseEnter() {
+		Color color = r.material.color;
+		color.a = 0.5f;
+		r.material.color = color;
+	}
+
+	void OnMouseExit() {
+		Color color = r.material.color;
+		color.a = 0.2f;
+		r.material.color = color;
+	}
+
+	void OnMouseDown() {
+	}
+
+	void OnMouseUp() {
+		gameObject.SetActive(false);
+		Instantiate(block, this.transform.position, this.transform.rotation);
+		//cam.camActive = true;
+		//transform.parent = null;
+	}
+}
