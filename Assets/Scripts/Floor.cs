118abae53ca0990693de41515180e5de5dfbb77e
CatsFromMars <mduque@andrew.cmu.edu>
2015-10-04 11:44:39 -0400

    Leap Motion added

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -5,6 +5,11 @@ public class Floor : MonoBehaviour {
 	private int resetCounter = 0;
 	private int resetWaitTime = 10;
 	public int blockCounter = 0;
+	private AudioSource a;
+
+	void Awake() {
+		a = GetComponent<AudioSource>();
+	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Block") {
@@ -17,6 +22,7 @@ public class Floor : MonoBehaviour {
 				GameState.ChangeState(GameState.State.Placing);
 
 			Destroy(other.gameObject);
+			if(!a.isPlaying) a.Play();
 		}
 	}
 
