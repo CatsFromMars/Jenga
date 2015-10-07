620a78cdba8cd5b8727183639700aa49872b6a45
CatsFromMars <mduque@andrew.cmu.edu>
2015-10-07 11:18:44 -0400

    cursorhand

---
+++ Assets/Scripts/FollowHand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowHand : MonoBehaviour {
+	public Transform handController; //Assign me
+	private GameObject hand;
+	
+	// Update is called once per frame
+	void Update () {
+		if (hand == null) {
+			hand = GameObject.FindGameObjectWithTag ("Palm");
+		} 
+		else
+			transform.position = hand.transform.position;
+	}
+}
